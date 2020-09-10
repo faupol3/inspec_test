b972fb9354294b2c01481769319f4f16a9e39dcb
zero.666.void <faustoignaciopolese@gmail.com>
2020-09-10 17:25:17 -0300

    first commit

---
+++ controls/test.rb
@@ -0,0 +1,11 @@
+title "My Test Controls"
+
+control "artifact-exists" do
+  impact 1
+  title "Artifact exists"
+  desc "Ensure that the artifact exists in the correct directory."
+  describe file("G:\\artifact-v1.zip") do
+    it { should exist }
+    it { should be_file }
+  end
+end
