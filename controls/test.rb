c7ff4dda9eb124e98a1f04b0f4523dbf113b7054
zero.666.void <faustoignaciopolese@gmail.com>
2020-09-10 19:09:34 -0300

    Fix artifact name

--- controls/test.rb
+++ controls/test.rb
@@ -4,7 +4,7 @@ control "artifact-exists" do
   impact 1
   title "Artifact exists"
   desc "Ensure that the artifact exists in the correct directory."
-  describe file("G:\\artifact-v1.zip") do
+  describe file("G:\\artifact_v1.zip") do
     it { should exist }
     it { should be_file }
   end
